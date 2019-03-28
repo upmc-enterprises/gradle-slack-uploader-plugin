3ac25feabf05550129757c98932771fd55cc54f2
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-03-28 15:31:04 -0400

    add in support for file uploads

--- src/main/kotlin/com/oliverspryn/gradle/SlackUploaderExtension.kt
+++ src/main/kotlin/com/oliverspryn/gradle/SlackUploaderExtension.kt
@@ -1,9 +1,12 @@
 package com.oliverspryn.gradle
 
 open class SlackUploaderExtension {
+
     var channels: List<String>? = null
     var comment: String? = null
+    var enabled = true
     var filePath: String? = null
+    var token: String? = null
 
     fun channels(vararg channels: String) {
         this.channels = channels.toList()
---
+++ src/main/kotlin/com/oliverspryn/gradle/exceptions/MissingChannelException.kt
@@ -0,0 +1,3 @@
+package com.oliverspryn.gradle.exceptions
+
+class MissingChannelException : Throwable("Please specify at least one channel to send the file")
